389dd2c02c0ec6a5be8fc9ec401387bec48c22a5
franco <francobustios07@gmail.com>
2025-11-09 13:25:18 -0600

    small changes

--- eval-c/main.c
+++ eval-c/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int weigh_hand(int * hand, int handsize) {
+    int * suits_freq = (int *)calloc(4, sizeof(int));
+    int * color_freq = (int *)calloc(2, sizeof(int));
+    int * values_freq = (int *)calloc(14, sizeof(int));
+    for (int i = 0; i < handsize; i++) {
+
+    }
+    free(suits_freq);
+    free(color_freq);
+    free(values_freq);
+    return 0;
+}
 
 int main(void) {
     printf("Hello, World!\n");
+
+
     return 0;
 }
