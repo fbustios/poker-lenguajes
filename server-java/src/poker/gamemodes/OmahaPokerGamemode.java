29f89d9844f6e1343ee0144a6e4671d56526ed13
franco <f.bustios.1@estudiantec.cr>
2025-12-01 07:07:20 +0000

    changes

---
+++ server-java/src/poker/gamemodes/OmahaPokerGamemode.java
@@ -0,0 +1,53 @@
+package poker.gamemodes;
+
+import poker.dealing.DealingMethod;
+import poker.items.Deck;
+import poker.items.Player;
+import poker.pot.Pot;
+import poker.pot.PotDistributer;
+import poker.rounds.HoldemRound;
+import poker.rounds.TurnManager;
+
+import java.util.Optional;
+
+public class OmahaPokerGamemode implements PokerGamemode {
+    private final Gamemode name = Gamemode.HOLD_EM;
+    private final DealingMethod dealingMethod;
+    private HoldemRound currentRound;
+    private final TurnManager turnManager;
+    private final PotDistributer potDistributer;
+    private final Pot pot;
+    private Deck deck;
+
+    public OmahaPokerGamemode(DealingMethod dealingMethod, TurnManager turnManager, PotDistributer potDistributer, Pot pot) {
+        this.dealingMethod = dealingMethod;
+        this.turnManager = turnManager;
+        this.potDistributer = potDistributer;
+        this.pot = pot;
+    }
+
+    @Override
+    public boolean isOver() {
+        return false;
+    }
+
+    @Override
+    public void play(PokerAction lastPokerAction) {
+
+    }
+
+    @Override
+    public Optional<Player> getNextTurn() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Gamemode getName() {
+        return null;
+    }
+
+    @Override
+    public void distributePot() {
+
+    }
+}
--- server-java/src/poker/rounds/Street.java
+++ server-java/src/poker/rounds/Street.java
@@ -1,5 +1,5 @@
 package poker.rounds;
 
 public enum Street {
-    THIRD, FOURTH, FIFTH, SIXTH, SEVENTH;
+    ANTE, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH;
 }
