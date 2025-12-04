e085edf0781606c750ed840bf558d7d57c019127
franco <f.bustios.1@estudiantec.cr>
2025-12-04 07:25:47 +0000

    changes

--- server-java/src/poker/items/Card.java
+++ server-java/src/poker/items/Card.java
@@ -1,8 +1,14 @@
 package poker.items;
 
 public record Card(Suit suit, Rank rank, boolean seen) {
+    public Card faceUp() {
+        return new Card(suit, rank, true);
+    }
+    public Card faceDown() {
+        return new Card(suit, rank, false);
+    }
     @Override
     public String toString() {
-        return suit.getRep() + rank.toString();
+        return seen ? suit.getRep() + rank.toString() : "??";
     }
 }
